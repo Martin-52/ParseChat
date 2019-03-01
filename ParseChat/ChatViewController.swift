d3639f88da648c5529e89805d8f99db71bf64b3e
Martin Landin <martinlandin@martins-macbook-air.local>
2019-02-28 17:19:50 -0800

    User able to log in/sign up. Can also send messages.

---
+++ ParseChat/ChatViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ChatViewController.swift
+//  ParseChat
+//
+//  Created by Martin Landin on 2/28/19.
+//  Copyright © 2019 Martin Landin. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSendButton(_ sender: Any) {
+        let chatMessage = PFObject(className: "Message")
+        chatMessage["text"] = messageTextField.text ?? ""
+        chatMessage.saveInBackground { (success, error) in
+            if success {
+                print("The message was saved!")
+                self.messageTextField.text = ""
+            } else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ParseChat/LoginViewController.swift
@@ -0,0 +1,105 @@
+//
+//  LoginViewController.swift
+//  ParseChat
+//
+//  Created by Martin Landin on 2/27/19.
+//  Copyright © 2019 Martin Landin. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignUpButton(_ sender: Any) {
+        // initialize a user object
+        let newUser = PFUser()
+        if(usernameText.text == "" || passwordText.text == "") {
+            let alertController = UIAlertController(title: "Error", message: "Username or Password is empty.", preferredStyle: .alert)
+            
+            // create an OK action
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // handle response here.
+            }
+            // add the OK action to the alert controller
+            alertController.addAction(OKAction)
+            present(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+            }
+        } else {
+            
+            // set user properties
+            newUser.username = usernameText.text
+            newUser.password = passwordText.text
+            
+            // call sign up function on the object
+            newUser.signUpInBackground { (success: Bool, error: Error?) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    let alertController = UIAlertController(title: "Error", message: "Error Signing Up.", preferredStyle: .alert)
+                    
+                    // create an OK action
+                    let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                        // handle response here.
+                    }
+                    // add the OK action to the alert controller
+                    alertController.addAction(OKAction)
+                    self.present(alertController, animated: true) {
+                        // optional code for what happens after the alert controller has finished presenting
+                    }
+                } else {
+                    print("User Registered successfully")
+                    // manually segue to logged in view
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        let username = usernameText.text ?? ""
+        let password = passwordText.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+                let alertController = UIAlertController(title: "Error", message: "Error Logging in.", preferredStyle: .alert)
+                
+                // create an OK action
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                    // handle response here.
+                }
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true) {
+                    // optional code for what happens after the alert controller has finished presenting
+                }
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
